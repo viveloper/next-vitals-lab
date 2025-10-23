e6fb068a344dc5a8b03a0d698046f5210ad69064
viveloper <ucsik1234@gmail.com>
2025-10-24 01:04:27 +1100

    perf(font): Optimize LCP by migrating @import to next/font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,11 @@
 // app/layout.tsx
 import './globals.css';
+import { Noto_Sans_KR } from 'next/font/google'
+
+const notoSansKR = Noto_Sans_KR({
+    weight: ["400", "900"],
+    subsets: ['latin'],
+})
 
 export const metadata = {
     title: '느린 페이지',
@@ -12,7 +18,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }) {
     return (
-        <html lang="en">
+        <html lang="en" className={notoSansKR.className}>
         {/* body 태그에 직접 폰트 클래스를 적용하지 않아
         next/font의 자동 최적화 이점을 받지 못하게 합니다.
       */}
